fb6423437820b0ab21fd9aeb5a8d24b953e4916a
Bienov Roman <rodergstep@gmail.com>
2016-08-17 11:34:29 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+                                          /*TASK 1*/
+function pow(x, n) {
+  var result = x;
+
+  for (var i = 1; i < n; i++) {
+    result *= x;
+  }
+  return result;
+}
+
+var x = prompt("Enter number", '');
+var n = prompt("Enter exponent", '');
+
+if (n < 1 ) {
+  alert('Ступінь ' + n +
+    ' не підтримується, введіть цілу ступінь, більше 1');
+} else if (parseInt(x) != x) {
+alert('Помилка. Введіть ціле число');
+} else {
+  alert(pow(x, n));
+}
+console.log(pow(x, n));
+                                          /*TASK 2*/
+/*var arrName = [];
+var userName;
+
+for (var i = 0; i < 5; i++) {
+    var userName = prompt('Enter any name:','');
+    arrName.push(userName);
+    }
+
+var login = prompt('Enter your name:','');
+
+function authorization(uName, fName) {
+    var lengthArr = fName.length;
+
+    for (var i = 0; i < lengthArr; i++) {
+        if (uName == fName[i]) {
+            alert(login + ", ви успішно ввійшли");
+            break;
+        } else {
+              alert("Помилка. Введений логін невірний")
+              break;
+        }
+    }
+}
+
+authorization(login, arrName);*/
